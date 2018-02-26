70cc516b3ccb35250ed7536e22797d239b758db9
Matt Trussell <smtrussell@gmail.com>
2018-02-26 04:26:17 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Matt!"
+  end
+
+
+  get '/hometown' do
+    "My hometown is Marble Falls!"
+  end
+
+
+  get '/favorite-song' do
+    "My favorite song is Ninja Rap!"
+  end
+
 end
