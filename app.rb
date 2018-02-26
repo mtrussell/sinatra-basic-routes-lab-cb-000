5842d500dd5e302eb3c8c44bb59f5cdc88f5d61e
Matt Trussell <smtrussell@gmail.com>
2018-02-26 04:26:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,17 +3,17 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    "My name is #{}"
+    "My name is Matt!"
   end
 
 
   get '/hometown' do
-    "My hometown is #{}"
+    "My hometown is Marble Falls!"
   end
 
 
   get '/favorite-song' do
-    "My favorite song is #{}"
+    "My favorite song is Ninja Rap!"
   end
-  
+
 end
